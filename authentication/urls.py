69c8b579fb737b07ba948e19aceb332fc6293a16
lorenzo campanile <lorenzo_campanile@virgilio.it>
2024-03-09 12:38:44 +0100

    improve - added authentication endpoint for a REST web client

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView
 
+from .views import ApiLoginView
+
 
 app_name = "authentication"
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
+    path('v2/login/', ApiLoginView.as_view(), name='login_v2'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
 ]
--- simpleaccountstorage/settings.py
+++ simpleaccountstorage/settings.py
@@ -42,6 +42,11 @@ SESSION_COOKIE_SECURE = not DEBUG
 
 CSRF_COOKIE_SECURE = not DEBUG
 
+# CSRF Trused Origins
+# https://docs.djangoproject.com/en/4.0/ref/settings/#csrf-trusted-origins
+
+CSRF_TRUSTED_ORIGINS = config['CSRF_TRUSTED_ORIGINS'].split(',')
+
 
 # Application definition
 
@@ -53,6 +58,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd party apps
+    "corsheaders",
+
     # Project apps
     'authentication.apps.AuthenticationConfig',
     'accountstorage.apps.AccountstorageConfig',
@@ -61,6 +69,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -154,3 +163,23 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Django REST Framework settings
+# https://www.django-rest-framework.org/api-guide/settings/
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
+
+
+# Django CORS headers allowed origins
+# https://pypi.org/project/django-cors-headers/
+if DEBUG:
+    CORS_ALLOWED_ORIGINS = [
+        'http://127.0.0.1:5173',
+    ]
+
+    CORS_ALLOW_CREDENTIALS = True
