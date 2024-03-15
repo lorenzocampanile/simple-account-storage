a003b0493e9c599c9de0e516e9848cc0eb1c0c91
lorenzo campanile <lorenzo_campanile@virgilio.it>
2024-03-15 22:10:59 +0100

    improve - added endpoint for obtaining an Account informations (with decrypted data)

--- accountstorage/views.py
+++ accountstorage/views.py
@@ -5,7 +5,9 @@ from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from rest_framework import viewsets
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from accountstorage.encrypt import decrypt_password
 
 from accountstorage.models import Account
 from .serializers import AccountSerializer
@@ -33,3 +35,15 @@ class AccountViewSet(viewsets.ModelViewSet):
         return Account.objects.prefetch_related('web', 'ssh', 'db')\
                               .filter(user=self.request.user)\
                               .order_by('label')
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        response_data = serializer.data
+
+        # Decode the password using the encryption key
+        encryption_key = request.META['HTTP_X_ENCRYPTION_KEY']
+        plain_text_password = decrypt_password(response_data['password'], encryption_key)
+        response_data['password'] = plain_text_password
+
+        return Response(response_data)
