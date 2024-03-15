fec3ed21b4aadf042107fea6accda9136ba75557
lorenzo campanile <lorenzo_campanile@virgilio.it>
2024-03-15 23:42:35 +0100

    trivial - changed paginator class

--- simpleaccountstorage/settings.py
+++ simpleaccountstorage/settings.py
@@ -176,7 +176,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
 
