41f69cc7989c8aa21a5e10622e2fb8ef1c582afd
lorenzo campanile <lorenzo_campanile@virgilio.it>
2024-03-17 21:10:13 +0100

    bugfix - fixed import which caused crash

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView
 
-from .views import ApiLoginView
-
-
 app_name = "authentication"
 urlpatterns = [
 ]
