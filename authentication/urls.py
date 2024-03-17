dd3a2faec2630c70aa405b1a299ab1df61cd926e
lorenzo campanile <lorenzo_campanile@virgilio.it>
2024-03-17 21:01:27 +0100

    improve - removed old login endpoints

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,8 +6,4 @@ from .views import ApiLoginView
 
 app_name = "authentication"
 urlpatterns = [
-    path('login/', LoginView.as_view(), name='login'),
-    path('v2/login/', ApiLoginView.as_view(), name='login_v2'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -3,19 +3,3 @@ from django.contrib.auth import login as auth_login
 from django.contrib.auth.views import LoginView, LogoutView, PasswordResetView
 
 from rest_framework import status
-
-
-class ApiLoginView(LoginView):
-    """
-    Default Django LoginView with some little customizations to make it
-    suitable for REST APIs contexts.
-    """
-    def form_valid(self, form):
-        """If the form is valid, return 200 OK code."""
-        auth_login(self.request, form.get_user())
-        return HttpResponse(status=status.HTTP_200_OK)
-
-    def form_invalid(self, form):
-        """If the form is invalid, return 401 Unauthorized code."""
-        print(form.errors)
-        return HttpResponse(status=status.HTTP_401_UNAUTHORIZED)
