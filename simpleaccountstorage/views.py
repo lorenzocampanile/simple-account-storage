ab346a57901f0f31126380d67af34032acce6cdf
lorenzo campanile <lorenzo_campanile@virgilio.it>
2024-03-24 00:45:15 +0100

    bugfix - fixed crash on the csrf token view

--- simpleaccountstorage/views.py
+++ simpleaccountstorage/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from django.views.generic import View
+from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import ensure_csrf_cookie
 
 
@@ -8,7 +9,7 @@ class CsrfTokenView(View):
     This endpoint provided a way for set the CSRF token when it's not
     already set.
     """
-    @ensure_csrf_cookie
+    @method_decorator(ensure_csrf_cookie)
     def get(self, request, *args, **kwargs):
         # All the work is made automatically by the CsrfViewMiddleware.
         # Just retrurn an empty JSON response.
